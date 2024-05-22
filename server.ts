cba523b77366346fd1b5efab1e5673c35b8b40bc
Trevor <trevorhilz@gmail.com>
2024-05-22 16:59:04 -0500

    removed localhost on console log

--- server.ts
+++ server.ts
@@ -33,5 +33,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
